72bb98eada3cd948947acfc4f23ed7bfff9062ef
iamvlnmurthy <iamvlnmurthy@gmail.com>
2026-02-12 18:11:35 +0530

    refactor: use DATABASE_URL in TypeORM for better connection pooler support

--- pepl-backend/src/app.module.ts
+++ pepl-backend/src/app.module.ts
@@ -22,11 +22,7 @@ import { DocumentsModule } from './documents/documents.module';
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
-        host: configService.get<string>('DB_HOST'),
-        port: configService.get<number>('DB_PORT') || 5432,
-        username: configService.get<string>('DB_USER'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_NAME'),
+        url: configService.get<string>('DATABASE_URL'),
         autoLoadEntities: true,
         synchronize: true,
         ssl: true,
